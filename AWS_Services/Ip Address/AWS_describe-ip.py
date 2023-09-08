1e8c1c21450b5717621bcebcf8f3c6361b2a0608
Anurag <anuragmahto85@outlook.com>
2023-09-08 23:30:13 +0530

    Wrong Folder Push

--- AWS_Services/Ip Address/AWS_describe-ip.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-ec2 = boto3.client('ec2')
-filters = [
-	{'Name':'domain', 'Values':['vpc']}
-]
-response = ec2.describe_addresses(Filters=filters)
-print("\n",response,"\n")
--- AWS_Services/Project/Aws-Project.py
+++
@@ -1,5 +0,0 @@
-
-// EC2 [launch, delete]
-// Bucket [create, list, delete]
-// KeyPairs [create, describe, delete]
-// IAM users [create, list, update, delete]
