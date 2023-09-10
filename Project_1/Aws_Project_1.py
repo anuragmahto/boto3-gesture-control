59eec80cf235394a473f1e2e6ac8b2cbf9e45358
Anurag <anuragmahto85@outlook.com>
2023-09-10 13:22:58 +0530

    Adding text

--- Project_1/Aws_Project_1.py
+++ Project_1/Aws_Project_1.py
@@ -29,10 +29,13 @@ while True:
 
 	if not ret:
 		break
+	
+	# text = "Press 'b' to exit"
+	# cv2.putText(photo,text,(10,50),cv2.FONT_HERSHEY_SIMPLEX,1,(127,255,212),2)
 
-	cv2.imshow("Display-Screen", photo)
-	if cv2.waitKey(1) & 0xff == ord('b'):
-		break
+	# cv2.imshow("Display-Screen", photo)
+	# if cv2.waitKey(1) & 0xff == ord('b'):
+	# 	break
 
 	hand_photo = detect.findHands(photo, draw=False)
 	if time.time() - start_time > delay:
@@ -45,39 +48,58 @@ while True:
 			start_time = time.time()
 
 			if fingerstatus == [1,0,0,0,0]:
-				print("\nLaunching an OS on Cloud.")
+				# print("\nLaunching an OS on Cloud.")
 				Launch_OS()
+				text = "Launching an Os on Cloud."
+				cv2.putText(photo,text,(10,50),cv2.FONT_HERSHEY_SIMPLEX,1,(127,255,212),2)
 
 			elif fingerstatus == [1,1,0,0,0]:
-				print("\nIam User Created.")
+				# print("\nIam User Created.")
 				user_create()
+				text = "Iam User Created."
+				cv2.putText(photo,text,(10,50),cv2.FONT_HERSHEY_SIMPLEX,1,(127,255,212),2)
 
 			elif fingerstatus == [1,1,1,0,0]:
-				print("\nIam User Updated.")
+				# print("\nIam User Updated.")
 				user_update()
+				text = "Iam User Updated."
+				cv2.putText(photo,text,(10,50),cv2.FONT_HERSHEY_SIMPLEX,1,(127,255,212),2)
 
 			elif fingerstatus == [0,1,0,0,0]:
-				print("\nS3 Bucket Created.")
+				# print("\nS3 Bucket Created.")
 				bucket_create()
+				text = "S3 Bucket Created."
+				cv2.putText(photo,text,(10,50),cv2.FONT_HERSHEY_SIMPLEX,1,(127,255,212),2)
 
 			elif fingerstatus == [0,1,1,0,0]:
-				print("\nS3 Bucket Removed.")
+				# print("\nS3 Bucket Removed.")
 				bucket_delete()
+				text = "S3 Bucket Removed."
+				cv2.putText(photo,text,(10,50),cv2.FONT_HERSHEY_SIMPLEX,1,(127,255,212),2)
 
 			elif fingerstatus == [0,1,1,1,0]:
-				print("\nEC2 Keys Created.")
+				# print("\nEC2 Keys Created.")
 				keys_create()
+				text = "EC2 Keys Created."
+				cv2.putText(photo,text,(10,50),cv2.FONT_HERSHEY_SIMPLEX,1,(127,255,212),2)
 
 			elif fingerstatus == [0,1,1,1,1]:
-				print("\nEC2 Keys Removed.")
-				keys_delete()
+				# print("\nUpdated User Removed.")
+				upd_user_delete()
+				text = "Iam Updated User Removed."
+				cv2.putText(photo,text,(10,50),cv2.FONT_HERSHEY_SIMPLEX,1,(127,255,212),2)
 
 			elif fingerstatus == [1,1,1,1,1]:
-				print("\nUpdated and Created user Removed.")
-				upd_user_delete()
+				# print("\nKeys and Created user Removed.")
+				keys_delete()
 				user_delete()
+				text = "Keys and Created User Removed."
+				cv2.putText(photo,text,(10,50),cv2.FONT_HERSHEY_SIMPLEX,1,(127,255,212),2)
 				
 			else:
 				delay = 0
+	cv2.imshow("Display-Screen", photo)
+	if cv2.waitKey(1) & 0xff == ord('b'):
+		break
 cap.release()
 cv2.destroyAllWindows()
